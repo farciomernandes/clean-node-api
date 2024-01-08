5d5cb03ff84dc71e10a4c43df844ac1de58396e6
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-08 17:28:56 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -20,10 +20,10 @@ const makeAddAccountRepositorie = (): AddAccountRepositorie => {
             const fakeAccount = {
                 id: 'valid_id',
                 name: 'valid_name',
-                email: 'valid_email',
+                email: 'valid_email@mail.com',
                 password: 'hashed_password',
             }
-            return new Promise(resolve => resolve(accountData))
+            return new Promise(resolve => resolve(fakeAccount))
         }
     }
 
@@ -110,5 +110,23 @@ describe('DbAddAccount Usecase', () => {
         const promise = sut.add(accountData);
 
         await expect(promise).rejects.toThrow();
+    })
+
+    test('Should return an account on success', async () => {
+        const { sut } = makeSut();
+
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_email@mail.com',
+            password: 'valid_password'
+        }
+        const account = await sut.add(accountData);
+
+        expect(account).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email@mail.com',
+            password: 'hashed_password'
+        })
     })
 })
