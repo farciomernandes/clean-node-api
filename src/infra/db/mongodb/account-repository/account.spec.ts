352644bc5f0d153b85465da514e51c17c306ba86
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-10 16:12:02 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,8 +11,18 @@ describe('Account Mongo Repository', () => {
         await MongoHelper.disconnect();
     });
 
+    beforeEach(async ()=> {
+       const accountCollection = await MongoHelper.getCollection('accounts');
+       await accountCollection.deleteMany({});
+    })
+
+
+    const makeSut = (): AccountMongoRepository => {
+        return new AccountMongoRepository();
+    }
+
     test('Deve retornar uma conta com sucesso', async () => {
-        const sut = new AccountMongoRepository();
+        const sut = makeSut();
 
         const account = await sut.add({
             name: 'any_name',
