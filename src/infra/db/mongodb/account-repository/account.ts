9de37063b76163877e6b86be5cc1703888c2e9d5
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-10 16:12:34 -0300

    feat: crete AccountMongoRepository

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,10 +1,10 @@
-import { AddAccountRepositorie } from "@/data/protocols/add-account-repositorie";
 import { AccountModel } from "@/domain/models/account";
 import { AddAccountModel } from "@/domain/usecases/add-account";
 import { MongoHelper } from "../helpers/mongo-helper";
+import { AddAccountRepository } from "@/data/protocols/add-account-repository";
 
 
-export class AccountMongoRepository implements AddAccountRepositorie {
+export class AccountMongoRepository implements AddAccountRepository {
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = MongoHelper.getCollection('accounts');
         const result = await accountCollection.insertOne(accountData);
