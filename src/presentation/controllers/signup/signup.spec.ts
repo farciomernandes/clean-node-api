39b194463d9578bb4027b4d968b5c6ded851ec6a
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-17 07:47:11 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -193,7 +193,7 @@ describe('SignUp Controller', () => {
         const httpResponse = await sut.handle(httpRequest);
 
         expect(httpResponse.statusCode).toBe(500);
-        expect(httpResponse.body).toEqual(new ServerError());
+        expect(httpResponse.body).toEqual(new ServerError(httpResponse.body.stack));
 
     })
 
@@ -238,7 +238,7 @@ describe('SignUp Controller', () => {
         const httpResponse = await sut.handle(httpRequest);
 
         expect(httpResponse.statusCode).toBe(500);
-        expect(httpResponse.body).toEqual(new ServerError());
+        expect(httpResponse.body).toEqual(new ServerError(httpResponse.body.stack));
 
     })
 
