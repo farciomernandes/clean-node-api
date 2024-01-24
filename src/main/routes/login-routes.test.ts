11c18296d7931c581eee1c8571de17f1667fa18a
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-23 22:26:05 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,6 +49,16 @@ describe('Login Routes', ()=> {
             })
             .expect(200)
         })
+
+        test('Should return 401 on login', async ()=> {
+            await request(app)
+            .post('/api/login')
+            .send({
+                email: 'marcio@email.com',
+                password: '123',
+            })
+            .expect(200)
+        })
     })
  
 })
