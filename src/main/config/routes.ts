3d73c8d776581e13e44ea05057bf512e0515293d
Marcio Fernandes <farciomernandes@gmail.com>
2024-01-24 07:33:25 -0300

    refactor: change routes to use filesystem and join insted of fast-glob

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,13 @@
 import { Express, Router } from 'express';
-import fastGlob from 'fast-glob';
+import { readdirSync } from 'fs';
+import { join } from 'path';
 
 export default (app: Express): void => {
-    const router = Router();
-    app.use('/api', router);
-    // fastGlob.sync -> retorna uma lista de string com a path completa com todos os arquivos que derem true abaixo
-    fastGlob.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
-}
+    const router = Router()
+    app.use('/api', router)
+    readdirSync(join(__dirname, '../routes')).map(async file => {
+      if (!file.endsWith('.map')) {
+        (await import(`../routes/${file}`)).default(router)
+      }
+    })
+  }
